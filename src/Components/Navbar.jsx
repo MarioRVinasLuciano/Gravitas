b9874e53edd2472e6c2559ad5950c6fc956fb4f7
bebitis <105821899+bebitis@users.noreply.github.com>
2024-04-06 18:15:35 -0400

    navbar md behavior changed to adjust to tablets

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -23,22 +23,22 @@ let Links = [
 
     return (
         <header className="bg-white top-0 w-full sticky z-10">
-            <div className="md:flex items-center justify-between py-4 ml-10 md:px-10 px-7 ">
+            <div className="md:flex items-center justify-between py-2 ml-10 md:px-10 px-7 ">
                 <div className="flex items-center pt-4">
                     <a href="/gravitas" className="text-3xl font-pirulen ">
                         <img className="h-12 w-26 " src={logo} alt="" />
                     </a>
                 </div>
-                <div className="text-2xl absolute right-8 top-10 cursor-pointer md:hidden" onClick={toggleMenu}>
+                <div className="text-2xl absolute right-8 top-10 cursor-pointer lg:hidden" onClick={toggleMenu}>
                     <span>
                         <img className="h-5 w-5" src={open ? close : menu} alt="Menu Icon"/>
                     </span>
                 </div>
                 <ul
-                    className={`md:z-auto z-[-1] left-0 md:mt-2 w-full md:w-auto md:flex md:items-center md:pb-0 md:pl-0 pl-9 pb-10 md:static absolute bg-white transition-all duration-500 ${open ? "" : "hidden"}`}
+                    className={`lg:z-auto z-[-1] md:top-20 md:items-center left-0  w-full lg:w-auto lg:flex lg:items-center lg:pb-0 lg:pl-0 pl-9 pb-10 lg:static absolute bg-white transition-all ease-in duration-500 ${open ? "" : "hidden"}`}
                 >
                     {Links.map((link) => (
-                        <li key={link.name} className="md:ml-6 mr-6 h-10 px-4 py-2 items-center text-lg md:my-0 my-7 hover:bg-slate-100 rounded-md md:">
+                        <li key={link.name} className=" mr-6 h-10 px-4 py-2 items-center text-lg  my-7 hover:bg-slate-100 rounded-md ">
                             <Link
                                 className="transform duration-300 hover:-translate-y-6 hover:scale-110 hover:text-gray-500 font-quicksand text-md"
                                 to={link.link} onClick={toggleMenu}
