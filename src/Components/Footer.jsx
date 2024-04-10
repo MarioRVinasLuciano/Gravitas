42bb1e72382613952ea807f24c8cecf1e96b7966
bebitis <105821899+bebitis@users.noreply.github.com>
2024-04-10 12:19:35 -0400

    fixed footer font

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,7 +7,7 @@ export function Footer() {
 <footer
   class="flex flex-col items-center text-center text-black pb-6 pt-10">
     <hr className="h-2 w-2/3 bg-grey-400"/>
-  <div class="w-full p-4 text-center font-pirulen">
+  <div class="w-full p-4 text-center font-quicksand">
     ©{current_year} Copyright Gravitas
   </div>
 </footer>
