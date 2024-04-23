930b11d2c9a6d7eccf05f78fecc826592f08495e
MarioRVinasLuciano <105821899+MarioRVinasLuciano@users.noreply.github.com>
2024-04-22 22:17:22 -0400

    fixed w issue with mobile devices

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -96,7 +96,7 @@ export default function Homepage() {
                 </div>
             </div>
             <div className="w-full px-8 sm:px-0 md:px-0 flex flex-col h-full  items-center bg-white gap-12 p-32" >
-                <div className="w-96 p-2 pt-6 pl-20 m-2"> 
+                <div className=" w-full p-2 pt-6 pl-20 m-2"> 
                     <h2 className="text-3xl font-quicksand font-bold">
                         Nuestro equipo
                     </h2>
